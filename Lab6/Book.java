8d1d1f482cfc543090b01e3524afd18cb563b3e2
enos23 <contras.enos@student.uoradea.ro>
2023-11-13 18:54:45 +0200

    Lab6

---
+++ Lab6/Book.java
@@ -0,0 +1,38 @@
+package Lab6;
+
+public class Book {
+    private String name;
+    private String author;
+    private String ISBN;
+
+
+    public Book(String name, String author, String ISBN) {
+        this.name = name;
+        this.author = author;
+        this.ISBN = ISBN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+}
--- Lab6/Main.java
+++ Lab6/Main.java
@@ -1,11 +1,38 @@
 package Lab6;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle(5);
-        Square square = new Square(6);
+//        Triangle triangle = new Triangle(5);
+//        Square square = new Square(6);
 
-        triangle.desen();
-        square.desen();
+//        triangle.desen();
+//        square.desen();
+
+        List<Book> listaDeCarti = new ArrayList<>();
+        List<String> listaDeStrings = new ArrayList<>();
+
+        listaDeCarti.add(new Book("Book1", "Author1", "1254374" ));
+        listaDeCarti.add(new Book("Book2", "Author2", "1254646" ));
+        listaDeCarti.add(new Book("Book3", "Author3", "1253255" ));
+
+        Book book1 = listaDeCarti.get(1); //
+
+        listaDeCarti.contains(book1);
+
+        listaDeCarti.forEach(element -> {
+            System.out.println(element.getName());
+            System.out.println(listaDeCarti.indexOf(element));
+
+        });
+
+        listaDeCarti.stream()
+                .filter(e -> e.getISBN().length() > 10)
+                .map(e -> e.getAuthor())
+                .toList();
     }
 }
